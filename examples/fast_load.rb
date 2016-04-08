a945f01c1ddae60033aebbdff0abff0ef9983f34
Michael Conrad <michael@threepears.com>
2016-04-08 15:37:11 -0500

    Add new tests and benchmark

--- examples/fast_load.rb
+++ examples/fast_load.rb
@@ -6,7 +6,7 @@ require 'benchmark'
 # add any needed username, password, port
 # install the required gems
 #
-# the easiest would to install bundler 
+# the easiest would to install bundler
 # > gem install bundler
 # > bundle install
 # > bundle exec fast_load.rb
@@ -45,3 +45,54 @@ encoder.remove
 #
 # 11.670000   0.010000  11.680000 ( 11.733414)
 #  0.030000   0.000000   0.030000 (  3.782371)
+
+
+
+# NEW TESTS BY MICHAEL CONRAD
+
+# Reset database and insert with .create
+ActiveRecord::Base.connection.execute %{
+  SET client_min_messages TO warning;
+  DROP TABLE IF EXISTS test_models;
+  CREATE TABLE test_models (id serial PRIMARY KEY, data VARCHAR);
+}
+
+puts "inserting into db via .create"
+puts Benchmark.measure {
+  TestModel.transaction do
+    1_000_000.times { TestModel.create(data: "test data") }
+  end
+}
+# Results on my 2.2 Ghz Intel Core i7 laptop connected to a postgres server.  It took 635.986367 seconds to insert 1,000,000 simple items into a table.
+#
+# 467.930000  30.920000 498.850000 (635.986367)
+
+
+
+# Reset database and insert with mass insert
+ActiveRecord::Base.connection.execute %{
+  SET client_min_messages TO warning;
+  DROP TABLE IF EXISTS test_models;
+  CREATE TABLE test_models (id serial PRIMARY KEY, data VARCHAR);
+}
+
+puts "inserting into db via single mass insert"
+
+thingy = []
+
+puts "adding data to array"
+puts Benchmark.measure {
+  0.upto(1_000_000).each {|i|
+    thingy.push "('test data')"
+  }
+}
+puts "inserting into db"
+puts Benchmark.measure {
+  TestModel.connection.execute "INSERT INTO test_models (data) VALUES #{thingy.join(', ')}"
+}
+# Results on my 2.2 Ghz Intel Core i7 laptop connected to a postgres server.  It took 18.338188 seconds to insert 1,000,000 simple items into a table.
+#
+#   0.330000   0.020000   0.350000 (  0.351006)
+#   0.140000   0.020000   0.160000 ( 18.338188)
+
+
